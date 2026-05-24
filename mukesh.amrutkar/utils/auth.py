48da02f544d49893c30504b9c74a20c8ac19c83c
mukesh853317 <mukeshamrutkar.shm@gmail.com>
2026-05-24 16:12:26 +0530

    Update auth.py

--- mukesh.amrutkar/utils/auth.py
+++ mukesh.amrutkar/utils/auth.py
@@ -1,16 +1,23 @@
 import streamlit as st
+import pandas as pd
+import os
 
 def show_login():
-    st.subheader("🔐 Mitradnya's Portal - Login")
+    st.subheader("🔐 Login")
     user = st.text_input("Username")
     password = st.text_input("Password", type="password")
     
     if st.button("Login"):
-        # हे फक्त टेस्टिंगसाठी आहे, नंतर आपण डेटाबेस वापरू
-        if user == "mukesh" and password == "123":
+        # CSV फाईल वाचणे
+        df = pd.read_csv('data/students.csv')
+        
+        # चेक करणे: युजर आणि पासवर्ड जुळतो का?
+        user_row = df[(df['Username'] == user) & (df['Password'].astype(str) == str(password))]
+        
+        if not user_row.empty:
             st.session_state.logged_in = True
             st.session_state.username = user
-            st.session_state.role = "Admin" # हा रोल आपण नंतर बदलू शकतो
+            st.session_state.role = user_row.iloc[0]['Role']
             st.rerun()
         else:
             st.error("Invalid Username or Password!")
