8287bd0317cbda0dc1eeeb0c56e102dc46c77446
mukesh853317 <mukeshamrutkar.shm@gmail.com>
2026-05-24 14:28:59 +0530

    Create auth.py

---
+++ mukesh.amrutkar/utils/auth.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+def show_login():
+    st.subheader("🔐 Mitradnya Portal - Login")
+    user = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    
+    if st.button("Login"):
+        # हे फक्त टेस्टिंगसाठी आहे, नंतर आपण डेटाबेस वापरू
+        if user == "mukesh" and password == "123":
+            st.session_state.logged_in = True
+            st.session_state.username = user
+            st.session_state.role = "Admin" # हा रोल आपण नंतर बदलू शकतो
+            st.rerun()
+        else:
+            st.error("Invalid Username or Password!")
